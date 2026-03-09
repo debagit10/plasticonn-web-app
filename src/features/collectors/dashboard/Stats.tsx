eb32cb6269dab73f48ec93e6e3b6d0f85fb9515e
Josiah Elias <josiahelias11@gmail.com>
2026-03-09 15:29:24 +0100

    feat: dashboard stats dynamically returned

--- src/features/collectors/dashboard/Stats.tsx
+++ src/features/collectors/dashboard/Stats.tsx
@@ -2,38 +2,60 @@ import { Typography } from "@mui/material";
 import co2saved from "../../../assets/co2saved.png";
 import submission from "../../../assets/submissions.png";
 import achievement from "../../../assets/Achievement.png";
-
-const stats = [
-  {
-    icon: co2saved,
-    stat: "+12.3%",
-    title: "Total CO₂ Saved",
-    value: "247.8 kg",
-    bg: "#00C2811A",
-    color: "#00C281",
-  },
-  {
-    icon: submission,
-    stat: "23 total",
-    title: "Verified Submissions",
-    value: "18",
-    bg: "#FA98081A",
-    color: "#FF9D0D",
-  },
-  {
-    icon: achievement,
-    stat: "Active",
-    title: "Achievement",
-    value: "3/12",
-    bg: "#8A38F51A",
-    color: "#8A38F5",
-  },
-];
+import { useEffect, useState } from "react";
+import api from "../../../utils/axiosInstance";
 
 const Stats = () => {
+  const [stats, setStats] = useState({
+    co2Saved: "0kg",
+    verifiedSubmissions: 0,
+    totalSubmissions: 0,
+    achievement: "Coming Soon",
+  });
+
+  const getStats = async () => {
+    try {
+      const response = await api.get("/api/collector/dashboard");
+      setStats(response.data.data);
+    } catch (error: any) {
+      console.log(error?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    getStats();
+  }, []);
+
+  const statsDisplay = [
+    {
+      icon: co2saved,
+      stat: "+12.3%",
+      title: "Total CO₂ Saved",
+      value: stats.co2Saved,
+      bg: "#00C2811A",
+      color: "#00C281",
+    },
+    {
+      icon: submission,
+      stat: `${stats.totalSubmissions} total`,
+      title: "Verified Submissions",
+      value: String(stats.verifiedSubmissions),
+      bg: "#FA98081A",
+      color: "#FF9D0D",
+    },
+    {
+      icon: achievement,
+      // stat: stats.achievement,
+      stat: "---",
+      title: "Achievement",
+      value: "Coming soon!!",
+      bg: "#8A38F51A",
+      color: "#8A38F5",
+    },
+  ];
   return (
     <div className="flex gap-10 mt-10">
-      {stats.map((stat, index) => (
+      {statsDisplay.map((stat, index) => (
         <div
           key={index}
           className="group bg-[#FAFAFA] p-10.5 rounded-xl shadow-[0_2px_6px_#1A1A1A26] w-125 h-82.5 transition-all duration-300"
