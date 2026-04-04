c460427ad7acde6e464715f059446f5304680f16
Josiah Elias <josiahelias11@gmail.com>
2026-04-04 09:58:41 +0100

    feat: back feature implemented from settings page

--- src/features/collection_center/CollectionCenter_Settings.tsx
+++ src/features/collection_center/CollectionCenter_Settings.tsx
@@ -4,12 +4,16 @@ import Profile_details from "./settings/Profile_details";
 import Accepted_Plastics from "./settings/Accepted_Plastics";
 import Edit_profile from "./modals/Edit_Profile";
 import DeleteAccount from "./modals/DeleteAccount";
+import { useNavigate } from "react-router-dom";
 
 const CollectionCenter_Settings = () => {
+  const navigate = useNavigate();
   return (
     <div className="px-16">
       <div className="flex items-center gap-3.5">
-        <img src={back} />
+        <div onClick={() => navigate("/dashboard")}>
+          <img src={back} className="cursor-pointer" />
+        </div>
 
         <div className="flex flex-col gap-3 mt-5">
           <Typography fontSize={28} fontWeight={400} color="#1A1A1A">
